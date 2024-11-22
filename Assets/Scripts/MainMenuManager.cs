dfe807cf8d6c1e5153d57e98cd8e4699ddd3a019
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-11-22 23:50:13 +0700

    add credits panel

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour {
     public string gameSceneName = "GameStage";
     public GameObject settingsPanel;
+    public GameObject creditsPanel;
 
     // Load the game scene to start the game
     public void StartGame() {
@@ -18,6 +19,17 @@ public class MainMenuManager : MonoBehaviour {
     // Close the settings panel in the main menu
     public void CloseSettings() {
         settingsPanel.SetActive(false);
+    }
+
+    // Open the credits panel in the main menu
+    public void OpenCredits() {
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    // Close the credits panel in the main menu
+    public void CloseCredits() {
+        creditsPanel.SetActive(false);
     }
 
     // Exit the game
